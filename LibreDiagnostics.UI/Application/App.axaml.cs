2ed32817aa1906b0bb19c102c117ea55327c018b
Blacktempel <6657796+Blacktempel@users.noreply.github.com>
2026-05-10 19:08:10 +0200

    Update for Avalonia 12. Update dependencies.

--- LibreDiagnostics.UI/Application/App.axaml.cs
+++ LibreDiagnostics.UI/Application/App.axaml.cs
@@ -80,10 +80,6 @@ namespace LibreDiagnostics.UI
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-                // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-                DisableAvaloniaDataAnnotationValidation();
-
                 ApplySystemTheme();
 
                 Current.PlatformSettings.ColorValuesChanged += (s, e) =>
@@ -102,19 +98,6 @@ namespace LibreDiagnostics.UI
         #endregion
 
         #region Private
-
-        void DisableAvaloniaDataAnnotationValidation()
-        {
-            // Get an array of plugins to remove
-            var dataValidationPluginsToRemove =
-                BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-            // remove each entry found
-            foreach (var plugin in dataValidationPluginsToRemove)
-            {
-                BindingPlugins.DataValidators.Remove(plugin);
-            }
-        }
 
         void ApplySystemTheme()
         {
